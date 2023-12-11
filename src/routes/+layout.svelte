07476b9a93ced5eeaa3ac610b2469cbe7b21657b
JacobLinCool <jacoblincool@gmail.com>
2023-12-12 03:14:01 +0800

    feat: banner and icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+	import { t } from "svelte-i18n";
 	import "../app.css";
 </script>
+
+<svelte:head>
+	<title>{$t("petal")}</title>
+	<meta name="description" content={$t("description")} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="og:image" content="/banner.png" />
+</svelte:head>
 
 <slot />
